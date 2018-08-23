5835f026375accdbc5bb7d193e015422072eecfc
Nate <nathanestes@gmail.com>
2018-08-23 17:29:47 -0600

    530 push

---
+++ Main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+
+//guess the color the computer is thinking of
+//player chooses from 3 random colors
+  //if player guesses on first try, they win,
+    //background changes to the correct color
+  //else computer wins 
+//program refreshes without reload
+  
+
+
+
+//Variables
+var randomWinningColor 
+var black = black //rgb value
+var red = red //rgb value
+
+var colorList = ["black", "red", "etc"] 
+  //add many more colors here
+  //text or rgb values?
+var random3Colors = ['','','']
+
+
+
+//Functions
+//pick 3 random colors from colorList
+  //pass 3 colors to new array
+  //also pass those 3 color names to 3 buttons
+
+  colorList.sample(random3Colors)
+
+
+
+
+
+
+
+
+
+
+
+})
